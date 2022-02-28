7a0e92c4ca1d759abf4f018deb2e4db898f4a4ce
Sơn Nguyễn <61671615+sonbill@users.noreply.github.com>
2022-02-28 23:46:44 +0700

    create new header

--- resources/views/user/layouts/app.blade.php
+++ resources/views/user/layouts/app.blade.php
@@ -4,7 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  {{-- CSS --}}
   <link href="/css/app.css" rel="stylesheet">
+  {{-- GOOGLE FONT - PT MONO --}}
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=PT+Mono&display=swap" rel="stylesheet">
+  {{-- FONTAWESOME --}}
+  <script src="https://kit.fontawesome.com/ea5c631906.js" crossorigin="anonymous"></script>
   <title>@yield('title')</title>
 </head>
 <body>
--- resources/views/user/layouts/header.blade.php
+++ resources/views/user/layouts/header.blade.php
@@ -1,5 +1,33 @@
-<nav>
-  <a href="#">Home</a>
-  <a href="#">About</a>
-  <a href="#">Contact</a>
+<nav class="bg-white">
+  <div class="max-w-7xl mx-auto px-2">
+    <div class="flex justify-between items-center">
+      {{-- LOGO --}}
+      <div>
+        <a href="#" class="text-lg font-bold py-4 px-3">FLIP.STUDIO</a>
+      </div>
+      {{-- PRIMARY NAV --}}
+      <div class="hidden md:flex space-x-3">
+        <a href="#" class="hover:underline text-gray-500 hover:text-gray-900 py-4 px-3 transition duration-100">COLLECTIONS</a>
+        <a href="#" class="hover:underline text-gray-500 hover:text-gray-900 py-4 px-3 transition duration-100">SHOP</a>
+        <a href="#" class="hover:underline text-gray-500 hover:text-gray-900 py-4 px-3 transition duration-100">ABOUT</a>
+      </div>
+      {{-- SECONDARY NAV --}}
+      <div class="hidden md:flex">
+        <a href="#" class="hover:underline text-gray-500 hover:text-gray-900 py-4 px-2 transition duration-100">ACCOUNT</a>
+        <a href="#" class="hover:underline text-gray-500 hover:text-gray-900 py-4 px-2 transition duration-100">BAG</a>
+      </div>
+      {{-- MOBILE BUTTON --}}
+      <div class="mobile-menu-button md:hidden flex items-center py-4">
+        <button>
+          <i class="far fa-bars w-6 h-6"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+  {{-- MOBILE MENU --}}
+  <div class="mobile-menu hidden">
+    <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-200">COLLECTIONS</a>
+    <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-200">SHOP</a>
+    <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-200">ABOUT</a>
+  </div>
 </nav>
